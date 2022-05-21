ee9caf021b5b2d7020294e0584058df58e4bf99d
Lil_Tim <99663083+LilTim0@users.noreply.github.com>
2022-05-21 21:48:50 +0200

    2022.05.21-21:47

--- example.py
+++ example.py
@@ -1,14 +1,17 @@
-from eezpass import *
+from eezpass import * # Import the eezpass module
 
-os.system('cls')
+os.system('cls') # Clear the console
 
-myPass = EezPass('admin')
+myPass = EezPass('admin') # Create a new instance of EezPass
 
-myPass.enable_logs(True)
-myPass.set_charsets('amns')
-myPass.set_length(16)
-myPass.enable_password_logs(True)
-myPass.include_string(True)
-myPass.set_include_string_position(0)
+myPass.enable_logs(True) # Enable general logs
+myPass.enable_password_logs(True) # Enable password logs
+myPass.set_charsets('amns') # Select all charsets
+myPass.set_length(16) # Set the password length to 16 characters
 
-myPass.print_password()
+myPass.include_string(True) # Enable string including
+myPass.set_include_string_position('*') # Set it to a random position
+
+myPass.print_properties() # Print properties
+myPass.print_password() # Print the final password
+myPass.print_passlogs('*') # Print all generated passwords
